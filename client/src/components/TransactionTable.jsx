7a9ed53ccaddf522299e8d0247ddd2933acc597b
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2024-09-16 00:30:40 +0700

    feat: add header button and transaction table

--- client/src/components/TransactionTable.jsx
+++ client/src/components/TransactionTable.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 
-// Function to format the date as DD/MM/YY
 const formatDate = (dateString) => {
   const options = { day: "2-digit", month: "2-digit", year: "2-digit" };
   return new Intl.DateTimeFormat("th-TH", options).format(new Date(dateString));
 };
 
+const userMap = {
+  1: "User 1",
+  2: "User 2",
+};
+
 const TransactionTable = ({ transactions }) => {
+  const getDescription = (transaction) => {
+    const lenderName = userMap[transaction.lender_id] || "Unknown Lender";
+    const borrowerName = userMap[transaction.borrower_id] || "Unknown Borrower";
+
+    if (transaction.transaction_type_id === 1) {
+      return `${lenderName} ยืมเงิน ${borrowerName}`;
+    } else if (transaction.transaction_type_id === 2) {
+      return `${lenderName} คืนเงิน ${borrowerName}`;
+    }
+    return "Unknown Transaction";
+  };
+
   return (
     <div>
       <h2 className="text-xl font-semibold mb-2">รายการยืม/คืนเงิน</h2>
@@ -30,12 +46,14 @@ const TransactionTable = ({ transactions }) => {
               <td className="text-black px-6 py-4 border-b">
                 {formatDate(transaction.timestamp)}
               </td>
-              <td className="px-6 py-4 border-b">{transaction.description}</td>
+              <td className="px-6 py-4 border-b text-gray-800">
+                {getDescription(transaction)}
+              </td>
               <td
                 className={`px-6 py-4 border-b text-right ${
                   transaction.transaction_type_id === 1
-                    ? "text-red-500"
-                    : "text-green-500"
+                    ? "text-green-500"
+                    : "text-red-500"
                 }`}
               >
                 {transaction.amount} บาท
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,6 +4,11 @@ const api = axios.create({
   baseURL: "http://localhost:8080/api",
 });
 
+export const fetchDebtSummary = async () => {
+  const response = await api.get(`/transactions`);
+  return response.data;
+};
+
 export const fetchTransactions = async (userId) => {
   const response = await api.get(`/transactions`, { params: { userId } });
   return response.data;
