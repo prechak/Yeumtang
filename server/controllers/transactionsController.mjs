4a3766d061e2f2c9370c7de71a4a998df7e20bd7
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2024-09-16 23:39:01 +0700

    refactor: update transaction query

--- server/controllers/transactionsController.mjs
+++ server/controllers/transactionsController.mjs
@@ -4,8 +4,30 @@ export const getTransactions = async (req, res) => {
   const { userId } = req.query;
 
   let transactionQuery = `
-      SELECT * FROM transactions
-      ORDER BY timestamp DESC`;
+      SELECT 
+    t.transaction_id,
+    u.user_id,
+    u.name AS user_name,
+    CASE 
+        WHEN t.lender_id = u.user_id THEN 'lender'
+        WHEN t.borrower_id = u.user_id THEN 'borrower'
+        ELSE 'none'
+    END AS user_role,
+    l.name AS lender_name,
+    b.name AS borrower_name,
+    t.amount,
+    t.timestamp AS timestampz,
+    t.transaction_type_id,
+    tt.name AS transaction_type_name,
+    t.lender_id,
+    t.borrower_id AS borrow_id
+FROM transactions t
+JOIN users l ON t.lender_id = l.user_id
+JOIN users b ON t.borrower_id = b.user_id
+JOIN users u ON (u.user_id = t.lender_id OR u.user_id = t.borrower_id)
+JOIN transaction_types tt ON t.transaction_type_id = tt.transaction_type_id
+WHERE t.lender_id = u.user_id OR t.borrower_id = u.user_id
+ORDER BY t.timestamp DESC;`;
 
   const queryParams = [];
 
